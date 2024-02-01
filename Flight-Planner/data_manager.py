668a059fab3bebb9f316b823b531f7a6b178d4f8
Ariel Zambeck <123191658+keepsake200@users.noreply.github.com>
2024-01-31 19:11:09 -0600

    Add files via upload

---
+++ Flight-Planner/data_manager.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import requests
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+SHEETY_PRICES_ENDPOINT = "SHEETY ENDPOINT HERE"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
